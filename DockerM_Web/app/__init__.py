37d31820f51bf75a4deacee820ca4a31c23059e2
mr9esx <1138099359@qq.com>
2017-02-07 17:51:51 +0800

    update runserver.py

--- DockerM_Web/app/__init__.py
+++ DockerM_Web/app/__init__.py
@@ -13,35 +13,35 @@ lm.login_view = 'dockermAuth.login'
 
 # 函数工厂，批量注册
 def create_app(config_name):
-	app = Flask(__name__)
-	app.config['SECRET_KEY'] = 'This is DockerM'
-	app.config.from_object(config[config_name])
-	config[config_name].init_app(app)
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'This is DockerM'
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
 
-	# 数据库链接设置
-	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-	db.init_app(app)
-	lm.init_app(app)
+    # 数据库链接设置
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    db.init_app(app)
+    lm.init_app(app)
 
-	#注册自定义函数init_app
-	env = app.jinja_env
-	env.filters['validataipaddress'] = validataipaddress
-	env.filters['cutStr'] = cutStr
-	env.filters['imageSize'] = imageSize
-	env.filters['stopRunTime'] = stopRunTime
-	env.filters['nowRunTime'] = nowRunTime
-	env.filters['formatPortsJson'] = formatPortsJson
-	env.filters['formatNetwrokJson'] = formatNetwrokJson
-	env.filters['showJsonPage'] = showJsonPage
-	env.filters['getImageName'] = getImageName
-	env.filters['getImageVer'] = getImageVer
-	env.filters['strptime2time'] = strptime2time
+    #注册自定义函数init_app
+    env = app.jinja_env
+    env.filters['validataipaddress'] = validataipaddress
+    env.filters['cutStr'] = cutStr
+    env.filters['imageSize'] = imageSize
+    env.filters['stopRunTime'] = stopRunTime
+    env.filters['nowRunTime'] = nowRunTime
+    env.filters['formatPortsJson'] = formatPortsJson
+    env.filters['formatNetwrokJson'] = formatNetwrokJson
+    env.filters['showJsonPage'] = showJsonPage
+    env.filters['getImageName'] = getImageName
+    env.filters['getImageVer'] = getImageVer
+    env.filters['strptime2time'] = strptime2time
 
-	# 注册蓝图
-	from .auth import dockermAuth as dockermAuth
-	from .main import dockerm as dockerm
-	from .api import dockermApi as dockermApi
-	app.register_blueprint(dockermAuth)
-	app.register_blueprint(dockerm)
-	app.register_blueprint(dockermApi)
-	return app
+    # 注册蓝图
+    from .auth import dockermAuth as dockermAuth
+    from .main import dockerm as dockerm
+    from .api import dockermApi as dockermApi
+    app.register_blueprint(dockermAuth)
+    app.register_blueprint(dockerm)
+    app.register_blueprint(dockermApi)
+    return app
--- DockerM_Web/runserver.py
+++ DockerM_Web/runserver.py
@@ -2,13 +2,18 @@
 import os
 from app import create_app, db
 from app.lib.dbModel import User
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell, Server
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 manager = Manager(app)
 migrate = Migrate(app, db)
+manager.add_command("runserver", Server(host="0.0.0.0", port=5000, use_debugger=True, threaded=True))
+
+@manager.command
+def creare():
+    pass
 
 if __name__ == '__main__':
     manager.run()
